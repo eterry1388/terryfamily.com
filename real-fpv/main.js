c61da85fb883e0ecffe216afa3b0e581e6f28d7e
Eric Terry <eterry1388@aol.com>
2025-01-05 03:20:52 -0600

    Add sky

--- real-fpv/main.js
+++ real-fpv/main.js
@@ -7,6 +7,7 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
 
 const cameraFar = 20000;
 const canvas = document.getElementById('webglCanvas');
@@ -17,6 +18,32 @@ renderer.setPixelRatio(window.devicePixelRatio);
 window.addEventListener('resize', onWindowResize, false);
 
 const scene = sceneSetup();
+
+// Add a nice sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+const effectController = {
+  turbidity: 10,
+  rayleigh: 2,
+  mieCoefficient: 0.005,
+  mieDirectionalG: 0.8,
+  elevation: 2,
+  azimuth: 240, // Changed azimuth to 0 to put the sun on the opposite side
+  exposure: renderer.toneMappingExposure
+};
+
+function updateSun() {
+  const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+  const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+  sun.setFromSphericalCoords(1, phi, theta);
+
+  sky.material.uniforms['sunPosition'].value.copy(sun);
+}
+
+updateSun();
 
 // Setting for controlling the camera wide-angle
 let cameraFov = 75;
@@ -40,7 +67,7 @@ const fxaaPass = new ShaderPass(FXAAShader);
 fxaaPass.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
 composer.addPass(fxaaPass);
 
-const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.5, 0.4, 2);
 composer.addPass(bloomPass);
 
 // Animate
--- real-fpv/sceneSetup.js
+++ real-fpv/sceneSetup.js
@@ -4,14 +4,14 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const baseUrl = `https://eric-terry-public.s3.amazonaws.com/terryfamily.com/real-fpv/worlds/`;
 
 const sceneAssets = {
-  sea_keep_lonely_watcher: { path: `${baseUrl}sea_keep_lonely_watcher.glb`, scale: 1.0, position: { x: 0, y: 0, z: 0 } },
-  cyberpunk_2077_night_city_3d_map: { path: `${baseUrl}cyberpunk_2077_night_city_3d_map.glb`, scale: 0.03, position: { x: 0, y: 0, z: 0 } },
-  tokyo_shinbashi: { path: `${baseUrl}tokyo_shinbashi.glb`, scale: 100.0, position: { x: 0, y: 0, z: 0 } },
-  jinshan_temple_cishou_pagoda: { path: `${baseUrl}jinshan_temple_cishou_pagoda.glb`, scale: 3.0, position: { x: 0, y: 0, z: 0 } },
-  k2: { path: `${baseUrl}k2.glb`, scale: 0.3, position: { x: 0, y: 0, z: 0 } },
-  desmoines1: { path: `${baseUrl}desmoines1.glb`, scale: 300, position: { x: 0, y: 500, z: 0 } },
-  desmoines: { path: `${baseUrl}desmoines.glb`, scale: 300, position: { x: 0, y: 500, z: 0 } },
-  desmoines3: { path: `${baseUrl}desmoines3.glb`, scale: 5, position: { x: 0, y: -1300, z: 0 } },
+  sea_keep_lonely_watcher: { path: `${baseUrl}sea_keep_lonely_watcher.glb`, scale: 1.0, position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  cyberpunk_2077_night_city_3d_map: { path: `${baseUrl}cyberpunk_2077_night_city_3d_map.glb`, scale: 0.03, position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  tokyo_shinbashi: { path: `${baseUrl}tokyo_shinbashi.glb`, scale: 100.0, position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  jinshan_temple_cishou_pagoda: { path: `${baseUrl}jinshan_temple_cishou_pagoda.glb`, scale: 3.0, position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  k2: { path: `${baseUrl}k2.glb`, scale: 0.3, position: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  desmoines1: { path: `${baseUrl}desmoines1.glb`, scale: 300, position: { x: 0, y: 500, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  desmoines: { path: `${baseUrl}desmoines.glb`, scale: 300, position: { x: 0, y: 500, z: 0 }, rotation: { x: 0, y: 0, z: 0 } },
+  desmoines3: { path: `${baseUrl}desmoines3.glb`, scale: 5, position: { x: 400, y: -1300, z: -600 }, rotation: { x: 0, y: 250, z: 0 } },
 }
 
 const currentScene = sceneAssets.desmoines3;
@@ -32,9 +32,8 @@ export function sceneSetup() {
   loader.load(currentScene.path, (gltf) => {
     const world = gltf.scene;
     world.scale.set(currentScene.scale, currentScene.scale, currentScene.scale);
-    world.position.x = currentScene.position.x;
-    world.position.y = currentScene.position.y;
-    world.position.z = currentScene.position.z;
+    world.position.set(currentScene.position.x, currentScene.position.y, currentScene.position.z);
+    world.rotation.set(currentScene.rotation.x, currentScene.rotation.y, currentScene.rotation.z);
     scene.add(world);
   }, undefined, (error) => {
     console.error('An error occurred while loading the GLB file:', error);
